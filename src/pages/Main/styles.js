e0c9371a31cd3923ba55a73178f1d783d51a5728
eduAlmeida1 <eduardo.almeida@cadmus.com.br>
2020-05-30 02:08:27 -0300

    utilizando estilizacao global

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-size: 24px;
-  color: ${(props) => (props.error ? 'red' : 'rgb(38, 37, 36)')};
-  font-family: Arial, Helvetica, sans-serif;
-
-  small {
-    font-size: 14px;
-    color: darkblue;
-  }
+  color: grey;
 `;
---
+++ src/styles/global.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html, body #root {
+    min-height: 100%;
+  }
+
+  body {
+    background: rgb(38, 37, 36);
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    color: #222;
+    font-size: 14px;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
