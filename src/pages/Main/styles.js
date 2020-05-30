d364ca39ad59eb4df8027d8a6b89f0b39fa73047
eduAlmeida1 <eduardo.almeida@cadmus.com.br>
2020-05-30 04:00:02 -0300

    listando repositorios

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   max-width: 700px;
   background: rgb(38, 37, 36);
   border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
   padding: 30px;
   margin: 80px auto;
 
@@ -70,4 +70,26 @@ export const SubmitButton = styled.button.attrs((props) => ({
         animation: ${rotate} 2s linear infinite;
       }
     `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #ede4e2;
+    }
+
+    a {
+      color: #ede4e2;
+      text-decoration: none;
+    }
+  }
 `;
